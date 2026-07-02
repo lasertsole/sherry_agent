65aa25794dff7c92a860fa600182d3ad9ee9a229
MOYE <56973455+lasertsole@users.noreply.github.com>
2026-07-02 15:51:01 +0800

    update pub_base

--- tools/pub_base/__init__.py
+++ tools/pub_base/__init__.py
@@ -3,6 +3,15 @@ from .file_utils import is_text_file, should_skip_dir
 from .text_matcher import fuzzy_find_and_replace, format_no_match_hint
 from .skill_usage import bump_patch, forget, mark_agent_created
 from .skill_provenance import is_background_review
+from .skill_utils import (
+    find_auto_skills,
+    sort_skills,
+    get_all_auto_skills_dirs,
+    parse_frontmatter,
+    iter_skill_index_files,
+    skill_matches_platform,
+    EXCLUDED_SKILL_DIRS,
+)
 
 __all__ = [
     "resolve_path",
@@ -13,5 +22,12 @@ __all__ = [
     "bump_patch",
     "forget",
     "mark_agent_created",
-    "is_background_review"
+    "is_background_review",
+    "find_auto_skills",
+    "sort_skills",
+    "get_all_auto_skills_dirs",
+    "parse_frontmatter",
+    "iter_skill_index_files",
+    "skill_matches_platform",
+    "EXCLUDED_SKILL_DIRS",
 ]
